28ad681cd0ede4ce85b3ca42d0aedcafe00de005
fullstack-guru0719 <mousesnake9@gmail.com>
2022-07-06 07:12:28 -0400

    first

---
+++ src/app/app.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from "@angular/core";
+import { Meta, Title } from "@angular/platform-browser";
+
+@Component({
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
+})
+export class AppComponent implements OnInit {
+
+  title: string = "Live Resume - Yoshida Ina";
+
+  constructor(
+    private titleService: Title,
+    private metaTagService: Meta
+  ) { }
+
+  ngOnInit(): void {
+
+    this.titleService.setTitle(this.title);
+
+    this.metaTagService.addTags([
+      { name: "keywords", content: "Web Developer, Full-stack Developer, DevOps Engineer, Android Developer, Yosida Ina Live Resume" },
+      { name: "robots", content: "index, follow" },
+      { name: "author", content: "Yosida Ina" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      { name: "date", content: "2020-05-12", scheme: "YYYY-MM-DD" },
+      { charset: "UTF-8" }
+    ]);
+
+    this.metaTagService.updateTag(
+      { name: "description", content: "Hello, I'm a Full-stack Engineer with 6+ years of experience designing web and mobile projects. Find out more in my live-resume!" }
+    );
+  }
+}
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,23 @@
+export const environment = {
+  production: true,
+  // replace the data below with your personal data
+  personal: {
+    caricature: "gbastos-illustration@2x-mask.png",
+    name: "Yoshida Ina",
+    birth: "1996-07-19",
+    email: "mousesnake9@gmail.com",
+    phone: "",
+    location: "Nagano, Itoshima-shi, FUKUOKA, JAPAN"
+  },
+  // replace the dummy data below with the real firebase configs
+  firebaseConfig: {
+    apiKey: "AIzaSyC2frVRAAltsVTmdwgFnATGHoVUzNHfzsM",
+    authDomain: "live-resume-a575a.firebaseapp.com",
+    databaseURL: "https://live-resume-a575a.firebaseio.com",
+    projectId: "live-resume-a575a",
+    storageBucket: "live-resume-a575a.appspot.com",
+    messagingSenderId: "681076751855",
+    appId: "1:681076751855:web:18bae3866ebfcc4fcd8a1a",
+    measurementId: "G-00VXD77WNG"
+  }
+};
